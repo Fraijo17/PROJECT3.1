917160a2caac331926b5d6e1151db9ae6f13b03b
Fraijo17 <abrahamfraijo17@gmail.com>
2021-03-03 20:48:08 -0700

    0

--- node-ex-website/server.js
+++ node-ex-website/server.js
@@ -8,13 +8,15 @@ app.use(express.static("express"));
 
 require("dotenv").config();
 
-
+app.get("/", function(_req, res) {
+    res.json(path.join(__dirname, "./express/index.html"));
+  });
 // default URL for website
-app.use('/', function(req,res){
+app.use('/', function(_req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
     //__dirname : It will resolve to your project folder.
   });
-const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port);
+  var server = app.listen(process.env.PORT || 5000, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
+  });
